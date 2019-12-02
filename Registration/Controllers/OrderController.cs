e8e6386117a5ee4f35a2eb7f22c17f032cb16935
Aga <Aga@laptop>
2019-12-02 13:47:32 +0000

    ViewModels/Order/ListOrderViewModel added

--- Registration/Controllers/OrderController.cs
+++ Registration/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Registration.Models;
+using Registration.ViewModels.Order;
 
 namespace Registration.Controllers
 {
@@ -17,11 +18,16 @@ namespace Registration.Controllers
         {
             List<Delivery> order = new List<Delivery>();
 
-            order.Add(new Delivery { DeliveryID = "0", DateTime = "20:30,12.12.2019", DeliveryAddress = "1 abc, Alfabet"});
-            order.Add(new Delivery { DeliveryID = "1", DateTime = "10:30,13.12.2019", DeliveryAddress = "2 Uknowkn, Somewhere 13-300" });
+            order.Add(new Delivery { DeliveryID = "0", DateTime = "20:30,12.12.2019", DeliveryAddress = "1 abc, Alphabet"});
+            order.Add(new Delivery { DeliveryID = "1", DateTime = "10:30,13.12.2019", DeliveryAddress = "2 Unknown, Somewhere 13-300" });
             order.Add(new Delivery { DeliveryID = "3", DateTime = "11:15,14.12.2019", DeliveryAddress = "13 Badluck, Hell" });
 
-            return View();
+            ListOrderViewModel viewModel = new ListOrderViewModel();
+
+            order.Add(new ListOrderViewModel { ProduceDate = "02.12.2019" }); //powinnam dodac string w clasie zeby dodal date?tylko ze to sa cyfry czyli int,a moze dodac gdzies "actual date"?
+
+       
+            return View(viewModel);
                 
                 
         }
---
+++ Registration/ViewModels/Order/ListOrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registration.ViewModels.Order
+{
+    public class ListOrderViewModel
+    {
+        public List<Delivery> Orders { get; set; }
+        public DateTime ProduceDate { get; set; }
+
+    }
+}
